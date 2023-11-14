84112da3540dda0b17d255d27b5d10d136be35c8
Zach Herridge <zachhe@amazon.com>
2023-11-14 10:02:14 -0800

    switch color scheme, add noe-tree, update install

--- install.sh
+++ install.sh
@@ -1,64 +1,74 @@
-Install homebrew
+#Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-Add homebrew to path (replace username)
+#Install git
+brew install git
+
+#Clone this repo
+git clone https://github.com/zach-herridge/.dotfiles ~/zenv
+
+#Add homebrew to path (replace username)
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/[username]/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-Install iterm2
+#Install iterm2
 brew install --cask iterm2
 
-Install fd
+#Install fd
 brew install fd
 
-Install tmux
+#Install tmux
 brew install tmux
 
-Install gh
+#Syslink tmux
+ln -s ~/zenv/.tmux.conf ~/
+
+#Install gh
 brew install gh
 
-Install tpm
+#Install tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-Install fzf
+#Install fzf
 brew install fzf
 
-Install ripgrep
+#Install ripgrep
 brew install ripgrep
 
-Install sqllite
+#Install sqllite
 brew install sqllite
 
-Install git
+#Install git
 brew install git
 
-Install oh-my-zsh
+#Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-Install powerlevel10k
+#Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-Configure powerlevel
-reopen iterm and/or p10k configure
-
-Set item color to the theme in this folder
-Go to Profiles > Colors > Import
-
-Install zh plugins
+#Install zh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-Syslink .zshrc
+#Syslink .zshrc
 ln -s ~/zenv/.zshrc ~/.zshrc
 
-Refresh term
-source ~/.zshrc
-
-Install neovim (install the latest version to get inlays)
+#Install neovim (install the latest version to get inlays)
 brew install --HEAD neovim
 
-Syslink nvm
+#Syslink nvm
 ln -s ~/zenv/nvim ~/.config
 
+#Manual steps
+
+#Configure powerlevel
+#reopen iterm and/or p10k configure
+
+#Set item color to the theme in this folder
+#Go to Profiles > Colors > Import
+
+#Install tmux plugins
+#enter tmux and run `ctrl + a I`
 
--- nvim/lua/zach/plugins/nvim-cmp.lua
+++ nvim/lua/zach/plugins/nvim-cmp.lua
@@ -57,26 +57,23 @@ return {
         { name = "nvim_lua" },
         { name = "nvim_lsp" },
         { name = "luasnip" },
-        { name = "buffer", keyword_length = 4 },
+        { name = "buffer",  keyword_length = 2 },
         { name = "path" },
       }),
       formatting = {
         format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
-          with_text = true,
-          menu = {
-            buffer = "[buf]",
-            nvim_lsp = "[lsp]",
-            nvim_lua = "[api]",
-            path = "[path]",
-            luasnip = "[snip]",
-          },
-        }),
-      },
-      experimental = {
-        ghost_text = true,
-      },
+          mode = 'symbol',   -- show only symbol annotations
+          maxwidth = 50,     -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+          ellipsis_char = '', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+          -- The function below will be called before any actual modifications from lspkind
+          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+          before = function(entry, vim_item)
+            vim_item.menu = entry:get_completion_item().detail
+            return vim_item
+          end
+        })
+      }
     })
   end,
 }
