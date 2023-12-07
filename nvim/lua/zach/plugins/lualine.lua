d0e5dcd274ca2ddb0dd8b7ccbff14d4900745605
Zach Herridge <zachhe@amazon.com>
2023-12-07 13:01:34 -0800

    add recording to lua line

--- nvim/lua/zach/plugins/lualine.lua
+++ nvim/lua/zach/plugins/lualine.lua
@@ -1,6 +1,15 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
+    local function show_macro_recording()
+      local recording_register = vim.fn.reg_recording()
+      if recording_register == "" then
+        return ""
+      else
+        return "Recording @" .. recording_register
+      end
+    end
+
     require('lualine').setup({
       options = {
         theme = "auto",
@@ -11,7 +20,13 @@ return {
       sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { { 'filename', path = 1 } },
+        lualine_c = {
+          { 'filename', path = 1 },
+          {
+            "macro-recording",
+            fmt = show_macro_recording,
+          }
+        },
         lualine_x = {},
         lualine_y = {},
         lualine_z = { 'location' }
